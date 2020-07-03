730238458f8cb5c392be95b2a7a145bfd3d22e5e
Thibault Ballier <me@tbmc.fr>
2020-07-03 16:05:51 +0200

    generate random files

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import os
 import secrets
 import random
 import string
-from typing import Optional
+from typing import Optional, Union, cast, Set
 
 
 name_length_limits = 5, 50
@@ -17,19 +17,41 @@ def generate_random(length: int) -> str:
     return ''.join(random.choices(string.ascii_letters, k=length))
 
 
-def generate_random_file(folder_path: str, extension: Optional[str], name_length: Optional[int], content_length: Optional[int]) -> None:
+def generate_random_file(folder_path: str, name: Union[int, str, None], content_length: Optional[int]) -> None:
     """
     This will overwrite file if it exists
     """
-    if not name_length:
-        name_length = random.randint(*name_length_limits)
-    name = generate_random(name_length)
+    if type(name) is str:
+        name = cast(str, name)
+    else:
+        if type(name) is int:
+            name = cast(int, name)
+            name = generate_random(name)
+        if not name:
+            name_length = random.randint(*name_length_limits)
+            name = generate_random(name_length)
+
     if not content_length:
         content_length = random.randint(*file_content_length_limits)
+
     content = generate_random_secure(content_length)
-    if extension:
-        name += "." + extension
+    name = cast(str, name)
     file_path = os.path.join(folder_path, name)
     with open(file_path, "wb") as file:
         file.write(content)
+
+
+def generate_random_files(folder_path: str, nbr_files: int, content_length: Optional[int]) -> None:
+    names: Set[str] = set()
+
+    for i in range(nbr_files):
+        name = ""
+        while len(name) == 0 and name not in names:
+            name_length = random.randint(*name_length_limits)
+            name = generate_random(name_length)
+
+        names.add(name)
+        generate_random_file(folder_path, name, content_length)
+
+
 
